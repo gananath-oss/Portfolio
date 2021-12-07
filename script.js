5df9b97bbe27322607b4115389b741253ef89972
gananath-oss <ganaya2k17@gmail.com>
2021-12-07 03:08:19 -0800

    Add teams deatais with 'https://cdnjs.com'

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTime: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                item: 1,
+                nav: false
+            },
+            600:{
+                item: 2,
+                nav: false
+            },
+            1000:{
+                item: 3,
+                nav: false
+            }
+        }
+    });
 });
